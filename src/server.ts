bbbc4f4d63d1b011549bf64c412ad029f2025734
GabrielGirotti <girottigabriel@gmail.com>
2024-08-12 05:43:14 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import express from "express";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
+import cors from "cors";
+import { corsConfig } from "./config/cors";
 import { connectDB } from "./config/bd";
-import shopRoutes from './routes/shopRoutes'
+import shopRoutes from "./routes/shopRoutes";
 
-dotenv.config()
-connectDB()
-const app = express()
-app.use(express.json())
-// Routes
+dotenv.config();
+connectDB();
+const app = express();
 
-app.use('/api/shops', shopRoutes)
+app.use(cors(corsConfig));
 
-export default app
+app.use(express.json());
+
+// Route
+app.use("/api/shops", shopRoutes);
+
+export default app;
