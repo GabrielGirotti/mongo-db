96eb8a22e60870d4cad946ebca2ce39506618e9a
GabrielGirotti <girottigabriel@gmail.com>
2024-08-13 09:52:44 -0300

    using morgan

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,15 @@ import cors from "cors";
 import { corsConfig } from "./config/cors";
 import { connectDB } from "./config/bd";
 import shopRoutes from "./routes/shopRoutes";
+import morgan from "morgan";
 
 dotenv.config();
 connectDB();
 const app = express();
 
 app.use(cors(corsConfig));
+
+app.use(morgan('dev'))
 
 app.use(express.json());
 
